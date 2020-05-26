15c88d563505908daa5275036d073e6cffaa8f59
enrico-kaack <e.kaack@live.de>
2020-05-26 22:03:03 +0200

    Added: Init File with Argument parsing

---
+++ main.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+
+
+
+class InputArguments:
+    INPUT_DIR = "inputDir"
+
+    @property
+    def inputDirectory(self):
+        return self._inputDirectory
+    
+    @inputDirectory.setter
+    def inputDirectory(self, value):
+        #TODO: check if it is a correct path
+        if os.path.isdir(value):
+            self._inputDirectory = os.path.abspath(value)
+        else:
+            raise FileNotFoundError("Input Dir is not a directory")
+
+    def __init__(self):
+        self.__parseInputArguments()
+    
+    def __parseInputArguments(self):
+        ap = argparse.ArgumentParser()
+        ap.add_argument(self.INPUT_DIR, metavar="<input directory>", type=str, nargs=1, 
+            help="Directory Path to be scanned for Python Source Files")
+        args = vars(ap.parse_args())
+
+        print("INPUT ARGS: ", args)
+
+        self.inputDirectory = args[self.INPUT_DIR][0]
+
+
+
+if __name__ == "__main__":
+    args = InputArguments()
